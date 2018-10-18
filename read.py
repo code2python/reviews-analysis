ad2e450757b64e8e39bdac38971cb22da3484473
Jerry Tseng <0817jerry@gmail.com>
2018-10-18 12:19:52 +1100

    add filter

--- read.py
+++ read.py
@@ -14,3 +14,16 @@ for d in data:
 	sum_len = sum_len + len(d)  ## sum_len += len(d)
 
 print('the average length of comment is', sum_len/len(data))
+
+
+## select records that comments is less than 100 characters
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+
+print('There are total', len(new), 'records that comment is less than 100 characters')
+print(new[0])
+print(new[1])
+
+
