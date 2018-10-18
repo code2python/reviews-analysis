dbfa498e6e86bd5825e1d3595fb534eadc08d060
Jerry Tseng <0817jerry@gmail.com>
2018-10-18 12:08:14 +1100

    calculate avg length'

--- read.py
+++ read.py
@@ -4,11 +4,13 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0: ## % --> 
+		if count % 10000 == 0: ## % --> 
 			print(len(data))
-
-print(data[0])
-print('---------------------------------------------------------------------------------')
-print(data[1])
+print('file reading has completed!! There are total', len(data), 'records found')
 
 
+sum_len = 0 
+for d in data:
+	sum_len = sum_len + len(d)  ## sum_len += len(d)
+
+print('the average length of comment is', sum_len/len(data))
